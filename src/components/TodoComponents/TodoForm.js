b5dece846475d0f10e0b2d7bbc5b00b43fab87e5
IsabellaGuo <isabella.guofei@gmail.com>
2020-01-13 15:41:33 -0800

    Todo Form is done

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,2 +1,41 @@
 import React from 'react';
+
+class TodoForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            newTodo: ""
+        };
+    }
+    
+    handleChanges = e => {
+        this.setState({
+            newTodo: e.target.value
+        });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.addTodo(this.state.newTodo);
+    };
+
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit}>
+            <div>
+                <input
+                  tyle="text"
+                  valye={this.state.newTodo}
+                  onChange={this.handleChanges}
+                  placeholder="Add to do here"
+
+                />
+                <button>Add</button>
+            </div>
+            <button>Clear All</button>
+            </form>
+        );
+    };
+};
+
 export default TodoForm;
